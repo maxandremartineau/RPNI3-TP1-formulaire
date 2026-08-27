d08d9f09be0e894e1540214feb902c37324aadc1
maxandremartineau <2463096@csfoy.ca>
2026-08-27 15:23:38 -0400

    Séparation de chaque étape

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,31 @@
-import './style.css'
-import heroImg from './assets/hero.png'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./css/style.css";
+let etape = 0;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const sections = document.querySelectorAll(".etape");
+const suivant = document.querySelectorAll(".suivant");
+const precedent = document.querySelectorAll(".precedent");
 
-<div class="ticks"></div>
+console.log(sections);
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+sections[0].classList.remove("hidden");
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+suivant.forEach((bouton) => {
+  bouton.addEventListener("click", () => {
+    sections[etape].classList.add("hidden");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    etape++;
+
+    sections[etape].classList.remove("hidden");
+  });
+});
+
+precedent.forEach((bouton) => {
+  bouton.addEventListener("click", () => {
+    sections[etape].classList.add("hidden");
+
+    etape--;
+
+    sections[etape].classList.remove("hidden");
+  });
+});
+
